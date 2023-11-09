679dc08560981a27834c24a55518724b2fb95565
TungPT12 <mylifeundermycontrol@gmail.com>
2023-11-10 01:31:42 +0700

    search form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,16 @@ import { DateRange } from "react-date-range";
 import { Link } from 'react-router-dom';
 import { vi } from 'date-fns/locale'
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPerson } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+    const [openOptions, setOpenOptions] = useState(false);
+    const [option, setOption] = useState({
+        adult: 0,
+        children: 0,
+        quantityRoom: 0,
+    })
     const [activePickerTime, setActivePickerTime] = useState(false);
     const { isAuthn } = useSelector(state => state.authn);
     const [date, setDate] = useState({
@@ -16,10 +24,50 @@ function Header() {
         endDate: new Date(),
         key: 'selection',
     });
-
     const formatDate = (date) => {
         return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     }
+
+    const handleOption = (optionType, isIncrease) => {
+        if (optionType === 'adult') {
+            if (isIncrease) {
+                setOption({
+                    ...option,
+                    adult: option.adult + 1
+                })
+            } else {
+                setOption({
+                    ...option,
+                    adult: option.adult - 1
+                })
+            }
+        } else if (optionType === 'children') {
+            if (isIncrease) {
+                setOption({
+                    ...option,
+                    children: option.children + 1
+                })
+            } else {
+                setOption({
+                    ...option,
+                    children: option.children - 1
+                })
+            }
+        } else {
+            if (isIncrease) {
+                setOption({
+                    ...option,
+                    quantityRoom: option.quantityRoom + 1
+                })
+            } else {
+                setOption({
+                    ...option,
+                    quantityRoom: option.quantityRoom - 1
+                })
+            }
+        }
+    }
+
 
     return (
         <div id="header" className=''>
@@ -28,7 +76,7 @@ function Header() {
                 <p className='mb-3'>Get rewarded for your travels-unlock instant saving of 10% or more with a free account</p>
                 {isAuthn ? <></> : <Link to="/login" className='header-button text-light border-0 h6 text-decoration-none'>Sign in / Register</Link>}
                 <div className="search-form position-relative d-flex justify-content-center">
-                    <form className='d-flex'>
+                    <div className='d-flex form'>
                         <div className='search-input__place f-1 d-flex align-items-center'>
                             <i className='fa fa-bed icon opacity-25'></i>
                             <input type='text' className='w-100' placeholder="Where are you going?" />
@@ -56,25 +104,67 @@ function Header() {
                                 />
                             </div>
                         </div>
-                        <div className='picker-quantity f-1 d-flex text-black align-items-center'>
-                            <i className='opacity-25 icon fa fa-female'></i>
+                        <div onClick={(e) => {
+                            e.stopPropagation();
+                            setOpenOptions(!openOptions)
+                        }} className='picker-quantity f-1 d-flex text-black align-items-center position-relative'>
+                            <FontAwesomeIcon icon={faPerson} className='opacity-25 icon me-2' />
+                            {/* <i className='opacity-25 icon fa fa-female'></i> */}
                             <div className='d-flex'>
-                                <p className='picker-quantity__holder opacity-25'>1 adult</p>
-                                <p className='picker-quantity__holder opacity-25'>0 children</p>
-                                <p className='picker-quantity__holder opacity-25'>1 room</p>
+                                <p className='picker-quantity__holder opacity-25'>{option.adult} adult ·</p>
+                                <p className='picker-quantity__holder opacity-25'>{option.children} children ·</p>
+                                <p className='picker-quantity__holder opacity-25'>{option.quantityRoom} room</p>
                             </div>
+                            {openOptions && (
+                                <div className="options" onClick={(e) => {
+                                    e.stopPropagation();
+                                }}>
+                                    <div className="optionItem">
+                                        <span className="optionText">Adult</span>
+                                        <div className="optionCounter">
+                                            <button onClick={() => {
+                                                handleOption('adult', false)
+                                            }} disabled={option.adult <= 0} className="optionCounterButton">-</button>
+                                            <span className="optionCounterNumber">{option.adult}</span>
+                                            <button onClick={() => {
+                                                handleOption('adult', true)
+                                            }} className="optionCounterButton">+</button>
+                                        </div>
+                                    </div>
+                                    <div className="optionItem">
+                                        <span className="optionText">Children</span>
+                                        <div className="optionCounter">
+                                            <button onClick={() => {
+                                                handleOption('children', false)
+                                            }} disabled={option.children <= 0} className="optionCounterButton">-</button>
+                                            <span className="optionCounterNumber">{option.children}</span>
+                                            <button onClick={() => {
+                                                handleOption('children', true)
+                                            }} className="optionCounterButton">+</button>
+                                        </div>
+                                    </div>
+                                    <div className="optionItem">
+                                        <span className="optionText">Room</span>
+                                        <div className="optionCounter">
+                                            <button onClick={() => {
+                                                handleOption('room', false)
+                                            }} disabled={option.quantityRoom <= 0} className="optionCounterButton" >-</button>
+                                            <span className="optionCounterNumber">{option.quantityRoom}</span>
+                                            <button onClick={() => {
+                                                handleOption('room', true)
+                                            }} className="optionCounterButton">+ </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                         <Link to="/search" state={{
                             startDate: date.startDate,
                             endDate: date.endDate
                         }} className='search-button text-decoration-none'>Search</Link>
-                        {/* <button onClick={(e) => {
-                            e.preventDefault();
-                            navigate('/admin')
-                        }} className='search-button text-decoration-none'>Search</button> */}
-                    </form>
+                    </div>
                 </div>
-            </div>
+            </div >
         </div >
     );
 }
--- src/components/SearchPopup/SearchPopup.jsx
+++ src/components/SearchPopup/SearchPopup.jsx
@@ -1,20 +1,88 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import styles from './SearchPopup.module.css';
-import { Card } from 'react-bootstrap';
-import CardFilter from './CardFilter/CardFilter';
+import styles from './SearchPopup.module.css';
+import './SubSearchPopup.css';
+import { DateRange } from "react-date-range";
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { format } from 'date-fns';
 
-function SearchPopup() {
+function SearchPopup(props) {
+    const location = useLocation();
+    const [date, setDate] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+    })
+    const [activePickerTime, setActivePickerTime] = useState(false);
+    // const [startDate, seStartDate] = useState(location.state?.startDate ? location.state?.startDate : new Date());
+    // const [endDate, setEndDate] = useState(location.state?.endDate ? location.state?.endDate : new Date());
+    // const selectionRange = {
+    //     startDate: startDate,
+    //     endDate: endDate,
+    //     key: 'selection',
+    // }
+
+    const formatDate = (date) => {
+        return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    }
 
     return (
-        <Card className={`f-1`}>
-            <h5 className={`px-2`}>Select by: </h5>
-            <CardFilter
-                title="Type"
-            />
-            <CardFilter
-                title="Area"
-            />
-        </Card>
+        <div className={`px-3 pb-4 pt-4 ${styles['search-popup']} `}>
+            <h3 className={`mb-1 ${styles['title-search-popup']}`}>Search</h3>
+            <div className={`mb-3  ${styles['destination']}`}>
+                <label className='mb-1 d-block'>Destination</label>
+                <input type='text' className='pt-2 pb-2' />
+            </div>
+            <div className={`mb-3 ${styles['checkin-date']}`}>
+                <label className='mb-1 d-block'>Check-in Date</label>
+                <div className={`${styles['picker-date']} w-100 bg-light position-relative d-flex text-black align-items-center`}
+                    onClick={(e) => {
+                        setActivePickerTime(!activePickerTime);
+                    }}
+                >
+                    <p className={`${styles['picker-date__holder']}`}>{format(date.startDate, 'dd/MM/yyyy')} to {format(date.endDate, 'dd/MM/yyyy')}</p>
+                    <div onClick={(e) => {
+                        e.stopPropagation();
+                    }}>
+                        <DateRange
+                            editableDateInputs={true}
+                            moveRangeOnFirstSelection={true}
+                            className={`date position-absolute w-100 search__popup__input-booking-day ${activePickerTime ? 'd-block' : 'd-none'}`}
+                            onChange={(e) => {
+                                setDate(e.selection);
+                            }}
+                            ranges={[date]}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className={` mb-4 ${styles['search-popup__option']}`}>
+                <label className={`mb-2`}>Options</label>
+                <div className={`mx-3 ${styles['input-option']}`}>
+                    <div className={`d-flex `}>
+                        <span>Min price per night</span>
+                        <input className="" />
+                    </div>
+                    <div className={`d-flex`}>
+                        <span>Max price per night</span>
+                        <input className={``} />
+                    </div>
+                    <div className={`d-flex d-`}>
+                        <span>Adult</span>
+                        <input type='number' />
+                    </div>
+                    <div className={`d-flex`}>
+                        <span>Children</span>
+                        <input type='number' />
+                    </div>
+                    <div className={`d-flex`}>
+                        <span>Room</span>
+                        <input type='number' />
+                    </div>
+                </div>
+            </div>
+            <button className={`w-100 pb-2 pt-2 ${styles['search-popup__button']}`}>Search</button>
+        </div >
     );
 }
 
