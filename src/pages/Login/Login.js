85b5c7f39735f99d50169ff28c8e8f61a321c1b5
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-27 09:31:55 +0700

    add modal loading in login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -45,39 +45,41 @@ function Login() {
     return (
         <>
             {
-                isLoadingModal ? <LoadingSpinnerModal /> : <div className={`${styles['login-wrap']} d-flex flex-column`}>
-                    <div className='' style={{
-                        backgroundColor: "#003580"
-                    }}>
-                        <div className={`${styles['navbar-container']} container`}>
-                            <Row className={`${styles['nav-bar_header']}`}>
-                                <Col><Link className='text-decoration-none text-light' to="/"><h2 className={`${styles['nav-bar__header__brand']}`}>Booking Website</h2></Link></Col>
-                                <Col className={`${styles[' nav-bar__header__action']} d-flex justify-content-end`}>
-                                    <Link to="/register" className={`${styles['button-navbar']} me-4 border-0 d-block bg-light text-decoration-none`}>Register</Link>
-                                    <Link to="/login" className={`${styles['button-navbar']} border-0 bg-light text-decoration-none`}>Login</Link>
-                                </Col>
-                            </Row>
-                        </div>
-                    </div>
-                    <div className={`h-100 w-100 d-flex justify-content-center align-items-center flex-column`}>
-                        <h1 className='text-center'>Login</h1>
-                        {wrongUser ? <p>Wrong username or password</p> : <></>}
-                        <form onSubmit={(e) => {
-                            e.preventDefault();
-                            setIsLoadingModal(true);
-                            onSubmitLogin();
-                        }} className={`${styles['login-form']} d-flex flex-column`}>
-                            <input type='text' required onChange={(e) => {
-                                setUserName(e.target.value)
-                            }} className='w-100 px-3 py-3 my-2' placeholder="User Name" />
-                            <input type='password' required onChange={(e) => {
-                                setPassword(e.target.value)
-                            }} className='w-100 px-3 py-3 my-2' placeholder="Password" />
-                            <button className={`${styles['login-action-btn']} py-2 px-2`}>Login</button>
-                        </form>
+                isLoadingModal ? <LoadingSpinnerModal /> : <></>
+            }
+            <div className={`${styles['login-wrap']} d-flex flex-column`}>
+                <div className='' style={{
+                    backgroundColor: "#003580"
+                }}>
+                    <div className={`${styles['navbar-container']} container`}>
+                        <Row className={`${styles['nav-bar_header']}`}>
+                            <Col><Link className='text-decoration-none text-light' to="/"><h2 className={`${styles['nav-bar__header__brand']}`}>Booking Website</h2></Link></Col>
+                            <Col className={`${styles[' nav-bar__header__action']} d-flex justify-content-end`}>
+                                <Link to="/register" className={`${styles['button-navbar']} me-4 border-0 d-block bg-light text-decoration-none`}>Register</Link>
+                                <Link to="/login" className={`${styles['button-navbar']} border-0 bg-light text-decoration-none`}>Login</Link>
+                            </Col>
+                        </Row>
                     </div>
                 </div>
-            }
+                <div className={`h-100 w-100 d-flex justify-content-center align-items-center flex-column`}>
+                    <h1 className='text-center'>Login</h1>
+                    {wrongUser ? <p>Wrong username or password</p> : <></>}
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        setIsLoadingModal(true);
+                        onSubmitLogin();
+                    }} className={`${styles['login-form']} d-flex flex-column`}>
+                        <input type='text' required onChange={(e) => {
+                            setUserName(e.target.value)
+                        }} className='w-100 px-3 py-3 my-2' placeholder="User Name" />
+                        <input type='password' required onChange={(e) => {
+                            setPassword(e.target.value)
+                        }} className='w-100 px-3 py-3 my-2' placeholder="Password" />
+                        <button className={`${styles['login-action-btn']} py-2 px-2`}>Login</button>
+                    </form>
+                </div>
+            </div>
+
         </>
     );
 }
